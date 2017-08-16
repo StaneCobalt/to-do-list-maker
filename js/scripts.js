596751829ea7b7bb937c8ad729c8fa47ceaf439e
StaneCobalt <scsladek91@gmail.com>
2017-08-16 16:28:07 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+function getTextBoxInfo(){
+  var textBoxText = document.getElementById('inputText').innerHTML;
+  var boxCounter = document.getElementsByTagName('input').length - 1;
+
+  var checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.name = "name";
+  checkbox.value = textBoxText;
+  checkbox.id = "checkBox" + boxCounter;
+
+  var label = document.createElement('label')
+  label.htmlFor = "id";
+  label.appendChild(document.createTextNode('text for label after checkbox'));
+
+  container.appendChild(checkbox);
+  container.appendChild(label);
+};
+
+function setHeaderText(){
+	var greeting = "Hello";
+	var currentHour = new Date().getHours();
+	if(currentHour < 8) { greeting = "Top of the Morning";}
+	else if(currentHour < 12) { greeting = "Good Morning"; }
+	else if(currentHour == 12) { greeting = "Good Day"; }
+	else if(currentHour > 12 && currentHour < 17) { greeting = "Good Afternoon"; }
+	else if(currentHour > 16 && currentHour < 24) { greeting = "Good Evening"; }
+	document.getElementById("mainHeader").innerHTML = greeting;
+};
+
+function getBackground(){
+	var backgrounds = 14;
+	var num = Math.ceil(Math.random() * backgrounds);
+	document.body.background = 'images/bg' + num + '.jpg';
+	document.body.style.backgroundSize = "cover";
+};
+
+function textEntered(){
+	
+};
+
+function setTimeKeeper(){
+	var goTime = new Date();
+	var hour = goTime.getHours();
+	if(hour > 12) hour = hour - 12;
+	var timeDisplayer = hour + ":" + goTime.getMinutes();
+	document.getElementById("timeKeeper").innerHTML = timeDisplayer;
+};
